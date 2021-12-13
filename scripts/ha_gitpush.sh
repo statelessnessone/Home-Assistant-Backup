586f82d79dd2427e31a8beb4c18e8020296a5961
root <root@core-ssh.local.hass.io>
2021-12-13 07:17:16 +0200

    config files on 13-12-2021 07:17:16

--- scripts/ha_gitpush.sh
+++ scripts/ha_gitpush.sh
@@ -7,6 +7,7 @@ git add .
 
 # Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+echo "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 
 # Push changes towards GitHub
 git push -u origin master
