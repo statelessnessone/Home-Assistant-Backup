8f301cbed7fd4d36aebcd4f0f2a8fdc8164ad612
Hardus <Hardus Engelbrecht>
2021-12-26 05:16:34 +0200

    config files on 26-12-2021 05:16:34

--- scripts/ha_gitpush.sh
+++ scripts/ha_gitpush.sh
@@ -2,14 +2,18 @@
 # Change this to your Home Assistant config folder if it is different
 cd /config
 
+timestmp = "`date +'%d-%m-%Y %H:%M:%S'`"
+
+echo "config file on " $timestmp
+echo '===================================='
 # Add all files to the repository with respect to .gitignore rules
 git add .
 
 # Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
-echo '===================================='
-echo "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 
 # Push changes towards GitHub
 git push -u origin master
 git push
+echo 'git push done'
+echo '===================================='
