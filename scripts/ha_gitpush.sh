e1331bf3513a2427a3fd0fcea7afd0349eda4011
Hardus <Hardus Engelbrecht>
2021-12-26 05:29:44 +0200

    $push_label

--- scripts/ha_gitpush.sh
+++ scripts/ha_gitpush.sh
@@ -1,19 +1,25 @@
+echo " "
 echo "===================================="
 
 # Go to /config folder or 
 cd /config
 
-push_label="`date +'%d-%m-%Y %H:%M:%S'`"
+
+push_label="config files on `date +'%d-%m-%Y %H:%M:%S'`"
 
 echo $push_label
+echo "------------------------------------"
+
 # Add all files to the repository with respect to .gitignore rules
 git add .
 
 # Commit changes with message with current date stamp
-git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+git commit -m '$push_label'
 
 # Push changes towards GitHub
 git push -u origin master
 git push
+
 echo "git push done!"
 echo "=============="
+echo 
